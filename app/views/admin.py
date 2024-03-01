9f56aec1dcd2d8791f2fd510750ce9a0eb793c70
dev-harshdeep <dev.harshjoshi@gmail.com>
2024-03-01 21:55:37 +0530

    Changes in dashboard

--- app/views/admin.py
+++ app/views/admin.py
@@ -1,30 +1,40 @@
-# app/admin.py
-
-from flask import Blueprint, render_template, session , redirect
-from views.auth import login_is_required  # Import the login_is_required decorator
-from views.auth import Authorised  # Make sure to import Authorised
-from functools import wraps
+import math
+import os
+from flask import Blueprint, render_template, session, redirect
+from models.question_paper import QuestionPaper
+from views.auth import login_is_required
+from datetime import datetime
 
 admin_bp = Blueprint('admin', __name__)
 
-# def login_is_required_admin(function):
-#     @wraps(function)
-#     def wrapper(*args, **kwargs):
-#         if "google_id" in session and "google_token" in session:
-#             if session["email"] in Authorised:
-                
-#                 return function()
-#         return redirect("/logout")
-#     return wrapper
+def get_last_backup_date():
+    # Suppose you retrieve the last backup date from a database
+    return datetime(2023, 5, 20, 15, 30, 0)  # Example date and time
 
 @admin_bp.route('/dashboard')
 @login_is_required
 def dashboard():
-    # if session.get('email') == 'admin@example.com':
-    return render_template('admin/dashboard.html')
-    # else:
-    #     return "You do not have permission to access the admin dashboard."
+    # Query the database to count the number of question papers
+    num_papers = QuestionPaper.query.count()
 
+    # Get the size of the folder where question papers are stored
+    folder_path =  os.path.join('/app/paperFiles')
+    folder_size = get_folder_size(folder_path)
+
+    # Convert folder size to a human-readable format (e.g., KB, MB, GB)
+    folder_size_gb =round( folder_size / (1024 * 1024 ),2)  # Convert bytes to MB
+
+    return render_template('admin/dashboard.html', num_papers=num_papers, folder_size_gb=folder_size_gb , last_backup_date=get_last_backup_date())
+
+def get_folder_size(folder_path):
+    # Get the size of the specified folder
+    folder_size = 0
+    for dirpath, _, filenames in os.walk(folder_path):
+        for filename in filenames:
+            file_path = os.path.join(dirpath, filename)
+            folder_size += os.path.getsize(file_path)
+        break  # We only need the size of the immediate folder, so break after the first iteration
+    return folder_size
 @admin_bp.route('/edit-course-info')
 @login_is_required
 def edit_course_info():    
