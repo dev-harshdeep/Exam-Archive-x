278eb6f0628db63ba9d44cf3125ea6714af625f6
Harshit Rajput <132146477+Harshit-Rajput20@users.noreply.github.com>
2024-03-02 00:19:09 +0530

    Changes in add/edit course  (#23)
    * added add/edit course functionality
    
    * changes in editCourse backend

--- app/views/editCourse.py
+++ app/views/editCourse.py
@@ -1,5 +1,10 @@
 from flask import Blueprint, render_template,request,jsonify,current_app
-
+from flask import request
+from models.course import Course
+from models.semester import Semester
+from models.subject import Subject
+from models.subject_semester import SubjectSemester
+from models.database import db
 # Create a blueprint instance
 edit_course_bp = Blueprint('edit_course', __name__)
 
@@ -8,14 +13,44 @@ edit_course_bp = Blueprint('edit_course', __name__)
 def index():
     # Render the index.html template
     return render_template('edit-course.html')
-
 @edit_course_bp.route('/backend_endpoint', methods=['POST'])
 def handle_form_data():
     data = request.json
-    # Process the received data here
-    current_app.logger.info(data)  # You can log the data to check if it's received correctly
+    if data:
+        course_name = data.get('courseName')
+        num_semesters = data.get('numSemesters')
+        all_semesters_data = data.get('allSemestersData')
 
-    # Here, you can perform further processing with the data, such as saving it to a database
-    
-    # Send a response back to the client
-    return jsonify({"message": "Data received successfully"}), 200
+        # Create a new course object and add it to the database
+        course = Course(CourseName=course_name)
+        db.session.add(course)
+        db.session.commit()
+
+        # Iterate over all semester data and create semester and subject objects
+        for semester_data in all_semesters_data:
+            semester_name = semester_data.get('name')
+            subjects_data = semester_data.get('subjects')
+
+            # Create a new semester object and associate it with the course
+            semester = Semester(CourseID=course.CourseID, SemesterName=semester_name)
+            db.session.add(semester)
+            db.session.commit()
+
+            # Iterate over subjects data and create subject objects
+            for subject_data in subjects_data:
+                subject_name = subject_data.get('name')
+                subject_code = subject_data.get('code')
+
+                # Create a new subject object
+                subject = Subject(Code=subject_code, SubjectName=subject_name)
+                db.session.add(subject)
+                db.session.commit()
+
+                # Associate the subject with the semester
+                subject_semester = SubjectSemester(SubjectID=subject.SubjectID, SemesterID=semester.SemesterID)
+                db.session.add(subject_semester)
+                db.session.commit()
+
+        return jsonify({"message": "Data received and saved successfully"}), 200
+    else:
+        return jsonify({"error": "No data received"}), 400
